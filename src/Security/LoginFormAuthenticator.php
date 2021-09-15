24bb9d1f6167435753d4c69c60bd38fd0553589e
Michal <matej50@gmail.com>
2021-09-15 14:06:05 +0200

    turn on user auth and added navbar

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -49,10 +49,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-    dump('here');
-        // For example:
         return new RedirectResponse($this->urlGenerator->generate('index'));
-//        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
